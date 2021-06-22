514c0802312dfe4942fa49e1160d41c24d8980f1
riflowth <riflowth@gmail.com>
2021-06-22 17:38:55 +0700

    Reuse db connection

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -7,28 +7,31 @@ if (!MONGO_USER || !MONGO_PASS || !MONGO_HOST || !MONGO_PORT || !MONGO_DB) {
 }
 
 const MONGO_URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`
-process.env.MONGO_URI = MONGO_URI
 
 // Prevents connections growing during API Route usage on ho reloads in development
 global.mongoose = global.mongoose || {}
 const cache = global.mongoose
 
 export default async function database(req, res, next) {
-  if (!cache.client) {
+  if (!cache.connection) {
     console.log('Connecting to MongoDB...')
     try {
-      cache.client = await mongoose.connect(MONGO_URI, {
+      const { connection } = await mongoose.connect(MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
         useFindAndModify: false
       })
+
+      cache.connection = connection
+      cache.client = connection.getClient()
+
       console.log('MongoDB connection established')
     } catch (error) {
       throw new Error(error)
     }
   }
 
-  //req.dbClient = cache.client
+  req.dbClient = cache.client
   return next()
 }
--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -1,7 +1,7 @@
 import session from 'express-session'
 import MongoStore from 'connect-mongo'
 
-const { SESSION_SECRET, MONGO_URI } = process.env
+const { SESSION_SECRET } = process.env
 
 if (!SESSION_SECRET) {
   throw new Error('Please define the SESSION_SECRET environment variable on .env.local')
@@ -12,6 +12,6 @@ export default function sessionMiddleware(req, res, next) {
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: MONGO_URI }),
+    store: MongoStore.create({ client: req.dbClient }),
   })(req, res, next)
 }
