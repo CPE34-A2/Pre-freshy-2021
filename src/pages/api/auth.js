9a7bc44b951b34903dc66235e1e1df83bdad58c7
riflowth <riflowth@gmail.com>
2021-06-23 18:02:28 +0700

    Refactor and db optimization

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -9,20 +9,26 @@ handler.use(middleware)
 handler.get((req, res) => {
   res.status(200).json({
     data: req.user,
-    message: req.isAuthenticated() ? 'logged in' : 'not logged in'
+    message: req.isAuthenticated() ? 'Logged in' : 'Not logged in'
   })
 })
 
 handler.post(
   passport.authenticate('local'),
   (req, res) => {
-    res.status(200).end()
+    res.status(200).json({
+      ...req.authInfo,
+      timestamp: new Date()
+    })
   }
 )
 
 handler.delete((req, res) => {
   req.logout()
-  res.status(200).end()
+  res.status(200).json({
+    message: 'Logged out successfully',
+    timestamp: new Date()
+  })
 })
 
 export default handler
