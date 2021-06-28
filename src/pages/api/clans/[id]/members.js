a0edc007c97faa24f9447541df3f35fac5dd6964
RiFlowTH <riflowth@gmail.com>
2021-06-28 19:05:38 +0700

    Use clan permission middleware

--- src/pages/api/clans/[id]/members.js
+++ src/pages/api/clans/[id]/members.js
@@ -1,6 +1,6 @@
 import nextConnect from 'next-connect'
 import middleware from '@/middlewares/middleware'
-import permission from '@/middlewares/permission/user'
+import permission from '@/middlewares/permission/clan'
 
 import Clan from '@/models/clan'
 import User from '@/models/user'
