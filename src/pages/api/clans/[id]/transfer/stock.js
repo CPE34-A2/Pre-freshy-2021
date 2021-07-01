af227bdb1508a20943c45d989c6b045c92bd1287
aonrok <aonrok555@gmail.com>
2021-07-02 02:45:28 +0700

    Rejected if the price changed during the transaction

--- src/pages/api/clans/[id]/transfer/stock.js
+++ src/pages/api/clans/[id]/transfer/stock.js
@@ -60,12 +60,12 @@ handler.post(async (req, res) => {
   if (amount <= 0)
     return Response.denined(res, 'amount must be greater than 0')
 
-  const duplicateTransaction = await Transaction.findOne({ $or: [{ 'owner.id': req.query.id, 'receiver.type': 'market' }, { 'receiver.id': req.query.id, 'owner.type': 'market' }] }, { 'status': 'PENDING' })
+  const pendingTransaction = await Transaction.findOne({ $or: [{ 'owner.id': req.query.id, 'receiver.type': 'market' }, { 'receiver.id': req.query.id, 'owner.type': 'market' }] }, { 'status': 'PENDING' })
     .select('_id')
     .lean()
     .exec()
 
-  if (duplicateTransaction)
+  if (pendingTransaction)
     return Response.denined(res, 'There are still pending transaction')
 
   const stock = await Stock
@@ -157,6 +157,18 @@ handler.patch(async (req, res) => {
   if (transaction.rejector.includes(req.user.id))
     return Response.denined(res, `Don't be indecisive. You can't confirm what you rejected.`)
 
+  const stock = await Stock
+    .findOne({'symbol': transaction.itme.stock.symbol})
+    .select('rate')
+    .lean()
+    .exec()
+
+  if (stock.rate != transaction.item.stock.rate) {
+    transaction.status = 'REJECT'
+    await transaction.save()
+    return Response.denined(res, `The price has been changed`)
+  }
+    
   transaction.confirmer.push(req.user.id)
   await transaction.save()
 
@@ -165,6 +177,7 @@ handler.patch(async (req, res) => {
     .select()
     .exec()
 
+  // If the number of confirmer equal expected required, then excute the transaction
   if (transaction.confirmer.length < transaction.confirm_require + 1)
     return Response.success(res, transaction)
 
@@ -173,6 +186,7 @@ handler.patch(async (req, res) => {
   if (transaction.owner.type === 'clan') {
     if (clan.properties.money < total) {
       transaction.status = 'REJECT'
+      await transaction.save()
       return Response.denined(res, 'no money, lol')
     }
     clan.properties.money -= total
@@ -182,6 +196,7 @@ handler.patch(async (req, res) => {
   } else if (transaction.owner.type === 'market') {
     if (clan.properties.stocks[transaction.item.stock.symbol] < transaction.item.stock.amount) {
       transaction.status = 'REJECT'
+      await transaction.save()
       return Response.denined(res, 'not enough stock, lol')
     }
     clan.properties.money += total
