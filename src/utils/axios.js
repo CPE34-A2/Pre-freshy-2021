669c36de6dbd6dbfbf28ed0ef647a2faffd6a66e
riflowth <riflowth@gmail.com>
2021-07-02 18:31:54 +0700

    Remove axios module

--- src/utils/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-axios.defaults.baseURL = process.env.WEB_URL || 'http://localhost:3000'
-axios.defaults.headers.post['Content-Type'] = 'application/json'
-
-export default axios
