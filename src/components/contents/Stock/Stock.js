ce397d7f7045fa9904bfa324d6f5c0241b1a20d3
riflowth <riflowth@gmail.com>
2021-07-04 19:20:16 +0700

    feat: add stock page

---
+++ src/components/contents/Stock/Stock.js
@@ -0,0 +1,11 @@
+import Dashboard from '@/components/common/Dashboard'
+
+export default function Stock({ user }) {
+  return (
+    <Dashboard current="stock" user={user} >
+
+      {/* Write your code here!!!! */}
+
+    </Dashboard>
+  )
+}
