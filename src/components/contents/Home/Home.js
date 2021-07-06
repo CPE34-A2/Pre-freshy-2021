ad9c6e07f0a279d4180e823faad5c82ff15ba649
riflowth <riflowth@gmail.com>
2021-07-06 23:59:45 +0700

    feat: responsive all devices (new layout) #25

--- src/components/contents/Home/Home.js
+++ src/components/contents/Home/Home.js
@@ -1,24 +1,43 @@
 import Dashboard from '@/components/common/Dashboard'
 import AssetsList from './AssetsList/AssetsList'
 import TaskList from './TaskList/TaskList'
+import NewsList from './NewsList/NewsList'
+import TransactionList from './TransactionList/TransactionList'
 
 export default function Home({ user, clan }) {
   return (
     <Dashboard current="home" user={user} clan={clan}>
-      <div className="flex flex-row w-full flex-wrap space-y-8 lg:space-y-12 xl:space-y-0 2xl:space-x-12 p-8 lg:p-12">
-          <div className="flex-grow">
+      <div className="w-full h-full flex flex-col xl:flex-row p-8 lg:p-12 space-y-8 lg:space-y-12 xl:space-y-0 xl:space-x-12">
+
+        <div className="flex flex-col space-y-8 lg:space-y-12 flex-shrink-0 flex-grow">
+          <div className="flex-shrink-0 lg:flex lg:flex-col xl:flex-row space-y-8 lg:space-y-12 xl:space-y-0 xl:space-x-12">
+            
             <AssetsList
               user={user}
               clan={clan}
             />
-          </div>
 
-          <div className="flex-grow">
             <TaskList
               user={user}
               clan={clan}
             />
           </div>
+
+          <div className="h-full w-full">
+            <TransactionList
+              user={user}
+              clan={clan}
+            />
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col flex-grow-0 flex-shrink">
+          <NewsList
+            user={user}
+            clan={clan}
+          />
+        </div>
+
       </div>
     </Dashboard>
   )
