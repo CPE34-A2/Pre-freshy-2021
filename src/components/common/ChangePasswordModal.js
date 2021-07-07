0a03c648ea139c3929a2062b63d893d3e777468c
aonrok <aonrok555@gmail.com>
2021-07-08 00:50:46 +0700

    feat: password changer page

--- src/components/common/ChangePasswordModal.js
+++ src/components/common/ChangePasswordModal.js
@@ -1,12 +1,64 @@
 import { useState } from 'react'
+
+import * as Util from '@/utils/common'
+import fetchAPI from '@/utils/fetch'
+import Router from 'next/router'
 import Modal from '@/components/common/Modal'
 import { CogIcon, XIcon } from '@heroicons/react/outline'
+import InputBox from '@/components/common/InputBox'
+import Button from '@/components/common/Button'
+import Spinner from '@/components/common/Spinner'
+import AlertNotification from '@/components/common/AlertNotification'
+
+
+import { EyeIcon, EyeOffIcon } from '@heroicons/react/outline'
+
+
 
 export default function NewsModal() {
   const [isOpen, setIsOpen] = useState(false)
+  const [oldPassword, setOldPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [isPasswordShowed, showPassword] = useState(false)
+  const [isLoggingIn, setLoggingIn] = useState(false)
+
+  const [changePasswordError, setChangePasswordError] = useState('')
 
   const openModal = () => setIsOpen(true)
   const closeModal = () => setIsOpen(false)
+
+
+  const handleOldPasswordChange = (e) => setOldPassword(e.target.value)
+  const handleNewPasswordChange = (e) => setNewPassword(e.target.value)
+
+  const MINIMUM_PASSWORD_LENGTH = 10
+
+  const changePassword = async (e) => {
+    e.preventDefault()
+
+    if (!oldPassword || !newPassword)
+      return setChangePasswordError('Please fill the field')
+
+    if (newPassword.length < MINIMUM_PASSWORD_LENGTH)
+      return setChangePasswordError('The new password must be atleast 10 characters')
+
+    setChangePasswordError(true)
+
+    fetchAPI('PATCH', '/api/auth', {
+      old_password: oldPassword,
+      new_password: newPassword
+    })
+      .then(async response => {
+        if (response.status == 200) {
+          setChangePasswordError('')
+          Router.push('/')
+        } else {
+          setChangePasswordError((await response.json()).message)
+        }
+      })
+  }
+
+
 
   return (
     <>
@@ -36,6 +88,63 @@ export default function NewsModal() {
               <h3 className="font-semibold text-2xl text-indigo-700 uppercase tracking-widest mt-2 mb-4">
                 CHANGE PASSWORD
               </h3>
+
+
+              <form className="mx-12 md:mx-auto ring-0 bg-white" onSubmit={changePassword}>
+                <div className="flex flex-col px-10 md:px-12 ">
+                  <div className="mb-5 pb-4 mx-auto">
+                  </div>
+
+                  <div className="mb-5">
+                    <p className="text-xs text-gray-800 font-semibold mb-1">Old Password</p>
+                    <InputBox
+                      type={isPasswordShowed ? 'text' : 'password'}
+                      style="w-full md:w-80 rounded-xl pr-7"
+                      value={oldPassword}
+                      onChange={handleOldPasswordChange}
+                      error={changePasswordError}
+                    />
+                    
+                  </div>
+
+                  <p className="text-xs text-gray-800 font-semibold mb-1">New Password</p>
+                  <div className="flex mb-5 relative">
+                    <InputBox
+                      type={isPasswordShowed ? 'text' : 'password'}
+                      style="w-full md:w-80 rounded-xl pr-7"
+                      value={newPassword}
+                      onChange={handleNewPasswordChange}
+                      error={changePasswordError}
+                    />
+                    <div className="absolute inset-y-0 mr-2 right-0 flex items-center">
+                      <button
+                        type="button"
+                        className="focus:outline-none"
+                        onClick={() => showPassword(!isPasswordShowed)}
+                      >
+                        {isPasswordShowed ? <EyeIcon className="h-4 w-4" /> : <EyeOffIcon className="h-4 w-4" />}
+                      </button>
+                    </div>
+                  </div>
+
+                  <AlertNotification
+                    type="error"
+                    info={changePasswordError}
+                  />
+
+
+
+                  <Button
+                    type="submit"
+                    name="CHANGE PASSWORD"
+                    icon={isLoggingIn && <Spinner style="mr-2 h-4 w-4 text-white" />}
+                    style={Util.concatClasses(
+                      "login-form-button inline-flex items-center justify-center mt-4 py-1 ring-0 rounded-3xl text-white text-sm font-semibold focus:outline-none",
+                    )}
+                  />
+                </div>
+              </form>
+
 
               <p className="text-xl text-gray-600 mb-4"></p>
             </div>
