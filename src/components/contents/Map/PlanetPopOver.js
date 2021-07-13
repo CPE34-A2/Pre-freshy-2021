3e9299ccf727a2d8be946b725468b977a37f4b62
eltfshr <puntf2@gmail.com>
2021-07-13 20:51:54 +0700

    feat: add visitied by at planet popover

--- src/components/contents/Map/PlanetPopOver.js
+++ src/components/contents/Map/PlanetPopOver.js
@@ -5,7 +5,7 @@ export default function PlanetPopover({ clan, planet, isHover }) {
         <div className="flex flex-col text-center absolute z-10 bg-white transform -translate-y-20 px-4 rounded-lg p-2 pointer-events-none">
           <div className="font-semibold text-indigo-800">{planet.name}</div>
           <div>ID: {planet._id}</div>
-          <div className="mt-1 text-gray-500">{planet.owner != 0 ? `Owned by Clan ${planet.owner}` : "Unoccupied"}</div>
+          <div className="mt-1 text-gray-500">{planet.owner != 0 ? `Owned by Clan ${planet.owner}` : planet.visitor != 0 ? `Visitied by Clan ${planet.visitor}` : "Unoccupied"}</div>
         </div>
       }
     </>
